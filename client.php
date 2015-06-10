8c2c5d23645a5c9e5b327e0bfefce3c1081fa126
Ron de Groot <ron@nandro.nl>
2015-06-10 09:52:45 +0200

    versie_26_11_2014

---
+++ client.php
@@ -0,0 +1,139 @@
+<?php
+	error_reporting(E_ALL ^ E_NOTICE);
+	require_once("signhost.php");
+	
+	if(isset ($_POST["createNewTransaction"])) {
+		$ondertekenen = new SignHost("https://api-staging.signhost.com", $_POST["appName"], $_POST["appKey"], $_POST["apiKey"]);
+		//$ondertekenen = new SignHost("https://api.signhost.com", $_POST["appName"], $_POST["appKey"], $_POST["apiKey"]);		
+	
+		$newTransaction = new Transaction($_FILES["file"]["name"], (bool)$_POST["seal"], $_POST["reference"], $_POST["postbackUrl"], $_POST["sendEmailNotifications"], $_POST["signRequestMode"], $_POST["daysToExpire"]);
+		
+		foreach($_POST["signers"] as $signer) {
+			$newTransaction->AddSigner($signer["email"], $signer["mobile"], (bool)$signer["requireScribble"], (bool)$signer["requireEmailVerification"], (bool)$signer["requireSmsVerification"], (bool)$signer["sendSignRequest"], $signer["signRequestMessage"], $signer["language"], $signer["scribbleName"], (bool)$signer["scribbleNameFixed"], $signer["reference"], $signer["returnUrl"], $signer["daysToRemind"]);
+		}
+		
+		$transaction = $ondertekenen->CreateTransaction($newTransaction);
+		$uploadResponse = $ondertekenen->UploadFileContent($transaction->File->Id, $_FILES["file"]["tmp_name"]);
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Ondertekenen.nl HTML Gateway</title>
+<style type="text/css">
+	td {
+		border: 1px solid black;
+		word-wrap: break-word;
+	}
+	th {
+		text-align: left;
+		border: 1px solid black;
+	}
+ </style>
+</head>
+
+<body>
+
+	<h1>Ondertekenen.nl API</h1>
+	
+	<form method="post" enctype="multipart/form-data">
+		
+		<fieldset>
+			<legend>Transaction</legend>
+			<input type="text" name="appName" value="" /> APP Name<br />
+			<input type="text" name="appKey" value="" /> APP Key<br />
+			<input type="text" name="apiKey" value="" /> API Key<br />
+			<input type="text" name="reference" value="" /> Reference<br />
+			<input type="text" name="postbackUrl" value="" /> Postback URL<br />
+			<input type="text" name="signRequestMode" value="2" /> Sign Request Mode<br />
+			<input type="text" name="daysToExpire" value="30" /> Days To Expire<br />
+			<input type="text" name="postbackUrl" value="" /> Postback URL<br />
+			<input type="checkbox" name="sendEmailNotifications" value="1" checked />Send Email Notifications<br /><br />
+			<div class="signerFieldsetContainer">
+				
+			</div>
+			<p><a href="" class="addSignerLink">Add signer</a></p>
+			<p>
+				<input type="file" name="file"> <input type="checkbox" name="seal" value="1" checked />Seal
+			</p>
+			<input type="submit" name="createNewTransaction" value="Create" />
+		</fieldset>
+		
+	</form>
+	
+	<fieldset id="signerFieldset" style="display: none;">
+		<legend>Signer <span class="signerNumber">1</span></legend>
+		<input type="text" name="signers[0][email]" value="" /> Email Receiver<br />
+		<input type="text" name="signers[0][mobile]" value="" /> Mobile Receiver<br />
+		<input type="text" name="signers[0][reference]" value="" /> Reference<br />
+		<input type="text" name="signers[0][returnUrl]" value="http://ondertekenen.nl" /> Return URL<br />
+		<input type="text" name="signers[0][scribbleName]" value="" /> Scribble Name<br />
+		<input type="text" name="signers[0][daysToRemind]" value="15" /> Days To Remind<br />
+		<p>
+			<input type="checkbox" name="signers[0][requireScribble]" value="1" />Require Scribble<br />
+			<input type="checkbox" name="signers[0][requireEmailVerification]" value="1" checked />Require Email Verification<br />
+			<input type="checkbox" name="signers[0][requireSmsVerification]" value="1" />Require Sms Verification<br />
+			<input type="checkbox" name="signers[0][sendSignRequest]" value="1" checked />Send Sign Request<br />
+			<input type="checkbox" name="signers[0][scribbleNameFixed]" value="1" />Scribble Name Fixed<br />
+			Language: <select name="signers[0][language]">
+				<option value="en-US">English</option>
+				<option value="nl-NL">Nederlands</option>
+				<option value="de-DE">Deutsch</option>
+				<option value="fr-FR">Français</option>
+				<option value="it-IT">Italiano</option>
+				<option value="es-ES">Español</option>
+			</select>
+		</p>
+		Sign Request Message:<br />
+		<textarea name="signers[0][signRequestMessage]" rows="4" cols="50">This is a test sign request.</textarea>
+	</fieldset>
+	
+	<?php 
+		if(isset($newTransaction)) {
+			var_dump($newTransaction);
+		}
+	?>
+	
+	<?php 
+		if(isset($transaction)) {
+			var_dump($transaction);
+		}
+	?>
+	
+	<?php 
+		if(isset($uploadResponse)) {
+			var_dump($uploadResponse);
+		}
+	?>
+	
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script>
+		
+		var signerCounter = 0;
+	
+		$(function() {
+			$('.addSignerLink').on('click', function(e) {
+				e.preventDefault();
+				
+				signerCounter++;
+				
+				var fieldset = $('#signerFieldset').clone();
+				
+				$('.signerNumber', fieldset).html(signerCounter);
+				
+				$('input, textarea', fieldset).attr('name', function(index, attr) {
+					return attr.replace(/\d+/, signerCounter-1);
+				});
+				
+				fieldset.insertAfter('.signerFieldsetContainer:last').removeAttr('id').show();
+				
+				
+				
+			});
+		});
+	</script>
+</body>
+
+</html>
---
+++ signhost.php
@@ -0,0 +1,132 @@
+<?php
+
+class SignHost
+{
+	public $ApiUrl;
+	public $AppName;
+	public $AppKey;
+	public $ApiKey;
+	public $SharedSecret;
+	
+	function __construct($apiUrl, $appName, $appKey, $apiKey, $sharedSecret = null)
+	{
+		$this->ApiUrl = $apiUrl;
+		$this->AppName = $appName;
+		$this->AppKey = $appKey;
+		$this->ApiKey = $apiKey;
+		$this->SharedSecret = $sharedSecret;
+	}
+	
+	public function CreateTransaction($transaction) {
+		$ch = curl_init($this->ApiUrl. "/api/transaction");
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($transaction));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Application: APPKey ". $this->AppName. " ". $this->AppKey, "Authorization: APIKey ". $this->ApiKey));
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,0);
+		$responseJson = curl_exec($ch);
+		return json_decode($responseJson);
+	}
+	
+	public function UploadFileContent($fileId, $filePath) {
+		$fh = fopen($filePath, 'r');
+		$ch = curl_init($this->ApiUrl. "/api/file/". $fileId);
+		curl_setopt($ch, CURLOPT_PUT, 1);
+		curl_setopt($ch, CURLOPT_INFILE , $fh);
+		curl_setopt($ch, CURLOPT_INFILESIZE , filesize($filePath));
+		curl_setopt($ch, CURLOPT_HEADER, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/pdf", "Application: APPKey ". $this->AppName. " ". $this->AppKey, "Authorization: APIKey ". $this->ApiKey));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,0);
+		$response = curl_exec($ch);
+		fclose($fh);
+		return $response;
+	}
+	
+	public function ValidateChecksum($masterTransactionId, $fileId, $status, $checksum) {
+		return sha1($masterTransactionId. "|". $fileId. "|". $status. "|". $this->SharedSecret) == $checksum;
+	}
+}
+
+class Transaction
+{
+	public $File;
+	public $Seal;
+	public $Signers;
+	public $Reference;
+	public $PostbackUrl;
+	public $SendEmailNotifications;
+	public $SignRequestMode;
+	public $DaysToExpire;
+	public $DaysToRemind;
+	
+	function __construct($fileName, $seal = 1, $reference = null, $postbackUrl = null, $sendEmailNotifications = true, $signRequestMode = 2, $daysToExpire = 30) {
+		$this->File = new File();
+		$this->File->Name = $fileName;
+		$this->Seal = $seal;
+		$this->Signers = array();
+		$this->Reference = $reference;
+		$this->PostbackUrl = $postbackUrl;
+		$this->SendEmailNotifications = $sendEmailNotifications;
+		$this->SignRequestMode = $signRequestMode;
+		$this->DaysToExpire = $daysToExpire;
+	}
+	
+	public function AddSigner($email, $mobile = null, $requireScribble = true, $requireEmailVerification = true, $requireSmsVerification = true, $sendSignRequest = true, $signRequestMessage = null, $language = null, $scribbleName = null, $scribbleNameFixed = true, $reference = null, $returnUrl = null, $daysToRemind = 15) {
+		$signer = new  Signer();
+		$signer->Email = $email;
+		$signer->Mobile = $mobile;
+		$signer->RequireScribble = $requireScribble;
+		$signer->RequireEmailVerification = $requireEmailVerification;
+		$signer->RequireSmsVerification = $requireSmsVerification;
+		$signer->SendSignRequest = $sendSignRequest;
+		$signer->SignRequestMessage = $signRequestMessage;
+		$signer->Language = $language;
+		$signer->ScribbleName = $scribbleName;
+		$signer->ScribbleNameFixed = $scribbleNameFixed;
+		$signer->Reference = $reference;
+		$signer->ReturnUrl = $returnUrl;
+		$signer->DaysToRemind = $daysToRemind;
+		$this->Signers[] = $signer;
+	}
+}
+
+class File
+{
+	public $Name;
+}
+
+class Signer
+{
+	public $Email;
+	public $Mobile;
+	public $RequireScribble;
+	public $RequireEmailVerification;
+	public $RequireSmsVerification;
+	public $SendSignRequest;
+	public $SignRequestMessage;
+	public $Language;
+	public $ScribbleName;
+	public $ScribbleNameFixed;
+	public $Reference;
+	public $ReturnUrl;
+}
+
+class AccessToken
+{
+	public $AccessToken;
+	public $ExpiresIn;
+	public $Username;
+	
+	function __construct($accessToken, $expiresIn, $username) {
+		$this->AccessToken = $accessToken;
+		$this->ExpiresIn = $expiresIn;
+		$this->Username = $username;
+	}
+}
+
+?>
