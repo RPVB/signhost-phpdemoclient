1a65f54385ffc1f3f2a07d739564f5dc244c39f3
Rowan <rowan@polderknowledge.nl>
2017-01-10 14:04:39 +0100

    added reference to status code, to function upload file content

--- src/SignHost.php
+++ src/SignHost.php
@@ -32,24 +32,26 @@ class SignHost
 		return json_decode($responseJson);
 	}
 
-	public function UploadFileContent($fileId, $filePath) {
-		$fh = fopen($filePath, 'r');
-		$ch = curl_init($this->ApiUrl. "/api/file/". $fileId);
-		curl_setopt($ch, CURLOPT_PUT, 1);
-		curl_setopt($ch, CURLOPT_INFILE , $fh);
-		curl_setopt($ch, CURLOPT_INFILESIZE , filesize($filePath));
-		curl_setopt($ch, CURLOPT_HEADER, 1);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/pdf", "Application: APPKey ". $this->AppName. " ". $this->AppKey, "Authorization: APIKey ". $this->ApiKey));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,0);
+    public function UploadFileContent($fileId, $filePath, &$statusCode) {
+        $fh = fopen($filePath, 'r');
+        $ch = curl_init($this->ApiUrl. "/api/file/". $fileId);
+        curl_setopt($ch, CURLOPT_PUT, 1);
+        curl_setopt($ch, CURLOPT_INFILE , $fh);
+        curl_setopt($ch, CURLOPT_INFILESIZE , filesize($filePath));
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/pdf", "Application: APPKey ". $this->AppName. " ". $this->AppKey, "Authorization: APIKey ". $this->ApiKey));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,0);
 
-		$response = curl_exec($ch);
+        $response = curl_exec($ch);
 
-		fclose($fh);
-		return $response;
-	}
+        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        fclose($fh);
+        return $response;
+    }
 
 	public function ValidateChecksum($masterTransactionId, $fileId, $status, $checksum) {
 		return sha1($masterTransactionId. "|". $fileId. "|". $status. "|". $this->SharedSecret) == $checksum;
